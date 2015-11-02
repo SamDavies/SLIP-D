5a0870b2692298d1cddf8463fd51b4a6b17abaf4
Sam Davies <samdavies@live.co.uk>
2015-11-02 15:34:32 +0000

    Test Lock Model

---
+++ ios/SmartLock/SmartLockTests/LockTest.swift
@@ -0,0 +1,42 @@
+//
+//  LockTest.swift
+//  SmartLock
+//
+//  Created by Sam Davies on 02/11/2015.
+//  Copyright © 2015 Sam Davies. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import Alamofire
+import SwiftyJSON
+@testable import SmartLock
+
+class LockTest: XCTestCase {
+    
+    var expectation: XCTestExpectation = XCTestExpectation()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.expectation = expectationWithDescription("Swift Expectations")
+        
+        LocksmithSmartLock.deleteUserPass()
+        LocksmithSmartLock.saveUserPass("tester@mail.com", pass: "python")
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        LocksmithSmartLock.deleteUserPass()
+    }
+    
+    func testLockList() {
+        Lock.getLockList().then {
+            locks -> Void in
+            XCTAssert(true)
+            self.expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
+}
--- ios/SmartLock/SmartLockTests/TransportSessionTest.swift
+++ ios/SmartLock/SmartLockTests/TransportSessionTest.swift
@@ -29,6 +29,7 @@ class TransportSessionTest: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        LocksmithSmartLock.deleteUserPass()
     }
 
     func testAPI() {

d3345ae2cb996b792bd9b569e9f173a711eabb03
Sam Davies <samdavies@live.co.uk>
2015-11-18 01:31:12 +0000

    Add open lock to view

--- ios/SmartLock/SmartLock/LockCell.swift
+++ ios/SmartLock/SmartLock/LockCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LockCell: UICollectionViewCell {
+class LockCell: UICollectionViewCell, UITableViewDelegate, UITableViewDataSource {
     
     var lock: Lock!
     @IBOutlet var openCloseButton: UIButton!
@@ -26,25 +26,81 @@ class LockCell: UICollectionViewCell {
         openCloseButton.layer.shadowOffset = CGSizeMake(12.0, 12.0)
     }
     
-    @IBAction func open(sender: AnyObject) {
-        // update the button on another thread
-        Lock.openLock(lock.id).then {
-            lock -> Void in
-            self.setWaiting()
+    func create(lock: Lock) {
+        self.lock = lock
+        lockName.text = lock.name
+        
+        // set initial state
+        updateButton()
+        
+        if(lock.requestedOpen && !lock.actuallyOpen){
             self.pollLockIsOpen(lock.id)
         }
+        
+        if(!lock.requestedOpen && lock.actuallyOpen){
+            self.pollLockIsClosed(lock.id)
+        }
+        
+//        User.getUserList(self.lock.id).then {
+//            users -> Void in
+//            
+//        }
+    }
+    
+    func updateButton(){
+        if(lock.requestedOpen && lock.actuallyOpen){
+            setOpen()
+        }else {
+            if(!lock.requestedOpen && !lock.actuallyOpen){
+                setClosed()
+            }else{
+                setWaiting()
+            }
+        }
+    }
+    
+    @IBAction func openClose(sender: AnyObject) {
+        // update the button on another thread
+        if(lock.requestedOpen && lock.actuallyOpen){
+            Lock.closeLock(lock.id).then {
+                lock -> Void in
+                self.lock = lock
+                self.updateButton()
+                self.pollLockIsClosed(lock.id)
+            }
+        }else{
+            if(!lock.requestedOpen && !lock.actuallyOpen){
+                Lock.openLock(lock.id).then {
+                    lock -> Void in
+                    self.lock = lock
+                    self.updateButton()
+                    self.pollLockIsOpen(lock.id)
+                }
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
     
     func pollLockIsOpen(id: Int) {
         Lock.getLock(id).then {
             lock -> Void in
-            if(lock.actuallyOpen == false){
+            self.lock = lock
+            self.updateButton()
+            if(!(lock.requestedOpen && lock.actuallyOpen)){
                 sleep(1)
+                debugPrint("waiting to open")
                 self.pollLockIsOpen(lock.id)
-            }else{
-                debugPrint("lock opened")
-                self.setOpen()
-                self.pollLockIsClosed(lock.id)
             }
         }
     }
@@ -52,13 +108,12 @@ class LockCell: UICollectionViewCell {
     func pollLockIsClosed(id: Int) {
         Lock.getLock(id).then {
             lock -> Void in
-            if(lock.actuallyOpen == true){
+            self.lock = lock
+            self.updateButton()
+            if(!(!lock.requestedOpen && !lock.actuallyOpen)){
                 sleep(1)
                 debugPrint("waiting to close")
                 self.pollLockIsClosed(lock.id)
-            }else{
-                debugPrint("closed")
-                self.setClosed()
             }
         }
     }
@@ -73,7 +128,6 @@ class LockCell: UICollectionViewCell {
     func setWaiting() {
         dispatch_async(dispatch_get_main_queue(), {
             // swap the open state
-            self.lock.requestedOpen = true
             self.openCloseButton.setTitle("...", forState: UIControlState.Normal)
         })
     }
@@ -81,15 +135,7 @@ class LockCell: UICollectionViewCell {
     func setOpen() {
         dispatch_async(dispatch_get_main_queue(), {
             // swap the open state
-            self.lock.requestedOpen = false
             self.openCloseButton.setTitle("O", forState: UIControlState.Normal)
         })
-    }
-    
-    func create(lock: Lock) {
-        self.lock = lock
-        lockName.text = lock.name
-        
-        setClosed()
     }
 }
